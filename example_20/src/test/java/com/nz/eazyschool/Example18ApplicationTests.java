522b0fceefa3e568cf0407c24834310e60685d1a
NZ <riceox@hotmail.com>
2023-08-09 19:50:58 +0200

    Eazyschool Homepage copy

---
+++ example_20/src/test/java/com/nz/eazyschool/Example18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nz.eazyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyschoolApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
