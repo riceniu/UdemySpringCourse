ed326ce9b623f4f58c1cd19e2fe6893d78ed6fa5
NZ <riceox@hotmail.com>
2023-08-18 14:05:34 +0200

    Validating the form input using Java Bean & Hibernate Validators

---
+++ example_24/src/main/java/com/nz/eazyschool/controller/ContactController.java
@@ -0,0 +1,60 @@
+package com.nz.eazyschool.controller;
+
+import com.nz.eazyschool.model.Contact;
+import com.nz.eazyschool.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Slf4j
+@Controller
+public class ContactController {
+    //private static Logger log = LoggerFactory.getLogger(ContactController.class);
+
+    @Autowired
+    private ContactService contactService;
+
+    @RequestMapping("/contact")
+    public String displayContactPage(Model model){
+        model.addAttribute("contact", new Contact());
+        return "contact.html";
+    }
+
+
+//    @PostMapping("/saveMsg")
+//    public ModelAndView saveMessage(@RequestParam(value="name") String name,
+//                                    @RequestParam String mobileNum,
+//                                    @RequestParam String email,
+//                                    @RequestParam String subject,
+//                                    @RequestParam String message){
+//        log.info("Name : " + name);
+//        log.info("Mobile Number : " + mobileNum);
+//        log.info("Email Address : " + email);
+//        log.info("Subject : " + subject);
+//        log.info("Message : " + message);
+//        return new ModelAndView("redirect:/contact");
+//
+//    }
+    @RequestMapping(value = "/saveMsg",method=POST)
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to :" + errors.toString());
+            return "contact.html";
+        }
+        contactService.saveMessageDetails(contact);
+        return "redirect:/contact";
+
+    }
+}
